121019e50d96fb20292c00f69a29a20938659ca3
paul lowe <perun@reagan.com>
2019-03-21 15:06:18 -0400

    adding hw files

---
+++ unit_1/w02d1/student_examples/lab_soutions.js
@@ -0,0 +1,9 @@
+//Conceptual Questions
+// use var/let/const
+//just change it with another var later on
+//= to assign variable
+var firstVariable = "Hello World"
+let firstVariable = 2
+let secondVariable = firstVariable
+let secondVariable = "winning";
+console.log(firstVariable)
