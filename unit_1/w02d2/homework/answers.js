8ae7a1e5ec799e833ffd20c3c95a6018d65958be
paul lowe <perun@reagan.com>
2019-03-23 10:54:53 -0400

    homework update

--- unit_1/w02d2/homework/answers.js
+++ unit_1/w02d2/homework/answers.js
@@ -40,3 +40,5 @@ for (let uppercase of ninjaTurtles) {
 //favMovies
 const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 console.log(favMovies[8])
+console.log(favMovies.sort())
+console.log(favMovies.pop())
