c20d5dd1121f549ba9f354ecb747926639511efa
paul lowe <perun@reagan.com>
2019-03-23 10:45:14 -0400

    correcting and adding files

---
+++ unit_1/w02d2/student_examples/array_practice.js
@@ -0,0 +1,33 @@
+// const faveFoods = ["icecream", "wings", "mac"];
+// console.log(faveFoods);
+// console.table(faveFoods)
+// console.log(faveFoods[0])
+// const ghostBusters = ["Venkman", "Spengler", "Stantz",
+//                         "Zeddemore", "Melnitz", "Barrett", "Tully"];
+//                         console.log(ghostBusters[0])
+//                         console.log(ghostBusters[2])
+//                         console.table(ghostBusters)
+//                         console.log(ghostBusters.length)
+//                         console.log(ghostBusters.length[4-1])
+
+// const veggies = ["red pepper", "leek", "pumpkin", "cauliflower"];
+// veggies[1] = "spinach";
+// console.log(veggies);
+// const numbers = [21, 18, 5, 3, 2, 1, 1];
+// numbers[2] = null
+// console.log(numbers)
+// numbers [0]= [numbers [0]*10]
+// console.log(numbers)
+// const drSeuss = ["Cat in the Hat", "Green Eggs and Ham", "Grinch",
+//               "Thing One", "Thing Two", "Cindy Loo Who", "JoJo McDodd"];
+//              console.log(drSeuss)
+//              for (let i = 0; i < drSeuss.length; i += 2) {
+//                console.log("Yo Dr S Whats Up: " + drSeuss[i]);
+//              };
+
+
+const looneyTunesChars = ["Bugs Bunny", "Daffy Duck", "Tweety",
+                        "Sylvester", "Elmer Fudd", "Porky Pig", "Taz"];
+                        for (let i = 1; i < looneyTunesChars.length; i += 2) {
+                        console.log("Waz up doc: " + looneyTunesChars[i]);
+                        };
