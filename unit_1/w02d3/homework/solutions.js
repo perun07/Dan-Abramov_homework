2cbce7d377f3518391ffed48b2a5f16b5b7d150c
paul lowe <perun@reagan.com>
2019-03-26 17:09:42 -0400

    prime finished awaiting game

--- unit_1/w02d3/homework/solutions.js
+++ unit_1/w02d3/homework/solutions.js
@@ -2,7 +2,7 @@
 //MY Commented Answer//
 // 1. Parameter is the variable in the declaration of the function. Argument is the actual value of this variable that gets passed to the fnction.
 // Return gives you the specified value. Console.log runs the function/item you have built.
-
+//checkPalindrome
 function checkPalindrome(palindrome) {
  const check = /[\W_]/g;
  //found regular expressions when looking on how to complete this. Super neat.
@@ -18,7 +18,7 @@ function checkPalindrome(palindrome) {
 console.log(checkPalindrome("Radar"));
 console.log(checkPalindrome("RACECAR"));
 console.log(checkPalindrome("Borscht"));
-
+//sumArray
 function sumArray(count) {
   if(!Array.isArray(count)) return;
   let total = 0;
@@ -27,5 +27,43 @@ function sumArray(count) {
   }
   return total;
 }
-
 console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+//Prime Numbers
+//checkPrime
+function checkPrime(prime) {
+for(var i = 2; i < prime; i++)
+    if(prime % i === 0) return false;
+  return prime > 1;
+}
+
+console.log(checkPrime (211));
+
+//printPrime
+function printPrime(n) {
+    var prime2 = [2];
+    for ( var i = 3; i < n; i+=2 ) {
+        if ( checkPrime(i) ) {
+            prime2.push(i);
+        }
+    }
+    console.log(prime2);
+}
+
+printPrime(100);
+
+//Rock Paper Scissors
+//Work to complete to finish. Will resubmit probably by tomorrow morning.
+// function radomMove
+// function rockPaperScissors
+//
+// let computersMove = randomMove();
+// => rock
+//
+// let usersMove = randomMove();
+// => paper
+//
+// rockPaperScissors(computersMove, usersMove);
+// => computer chose rock
+// => user chose paper
+// => paper beats rock, user wins!
