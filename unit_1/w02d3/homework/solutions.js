fe5b9cb9c3cc542232130eaf70534cbcbd33cf30
paul lowe <perun@reagan.com>
2019-03-26 13:40:34 -0400

    Palindrome

--- unit_1/w02d3/homework/solutions.js
+++ unit_1/w02d3/homework/solutions.js
@@ -2,3 +2,20 @@
 //MY Commented Answer//
 // 1. Parameter is the variable in the declaration of the function. Argument is the actual value of this variable that gets passed to the fnction.
 // Return gives you the specified value. Console.log runs the function/item you have built.
+
+function checkPalindrome(palindrome) {
+ const check = /[\W_]/g;
+ //found regular expressions when looking on how to complete this. Super neat. 
+ palindrome = palindrome.toLowerCase().replace(check, '');
+ const leng = palindrome.length;
+ for (let i = 0; i < leng/2; i++) {
+   if (palindrome[i] !== palindrome[leng - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
+
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("RACECAR"));
+console.log(checkPalindrome("Borscht"));
